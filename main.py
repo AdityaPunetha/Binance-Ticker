b47ef98da89fc3c4462ff4c0ad58aa9ed4591b27
AdityaPunetha <adityapunetha6@gmail.com>
2021-06-12 15:37:12 +0530

    done just loop left

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import discord
 import json
-from discord.ext import commands
 
 
 def get_eth_price():
@@ -20,30 +19,14 @@ async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
 
-'''
+# message.channel.send(name)
+#message.guild.me.edit(nick=name)
+
 @client.event
-
 async def on_message(message):
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hello!")
-    if message.content.startswith('$o'):
-        await message.channel.send(message.author)
-    if message.content.startswith('$n'):
-        await message.channel.send(client.user)
-    if message.content.startswith('$k'):
-        await message.channel.send(client.guilds)
-    if message.content.startswith('$g'):
-        await client.change_presence(status="get_eth_price()")
-'''
-bot = commands.Bot(command_prefix="!!")
-
-
-@bot.command()
-async def run(ctx):
-    while True:
-        change = get_eth_price()
-        # name="S&P500: \n$"+ str(round(si.get_live_price("StockSymbol"),2))+ "("+str(round(change["regularMarketChangePercent"],2))+")"+"%"
-        await ctx.guild.me.edit(nick=change)
+    name = get_eth_price()
+    # if message.content.startswith('e'):
+    await message.guild.me.edit(nick=name)
 
 
 client.run(tk)
