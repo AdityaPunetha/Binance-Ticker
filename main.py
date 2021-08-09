5dfdfb2b56b11cf9ec3c997c619d852fece22cc1
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2021-08-09 15:24:02 +0530

    status done

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import requests
 import discord
 import json
+from discord.ext.tasks import loop
 
 
-def get_eth_price():
+def get_price():
     a = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT')
     json_data = json.loads(a.text)
     price = round(float(json_data['price']), 3)
     return price
 
 
+def get_change():
+    a = requests.get('https://api.binance.com/api/v3/ticker/24hr?symbol=ETHUSDT')
+    json_data = json.loads(a.text)
+    per = str(
+        "{0} ({1})".format(round(float(json_data['priceChange']), 3), round(float(json_data['priceChangePercent']), 2)))
+    return per
+
+
 client = discord.Client()
-print(get_eth_price())
+print(get_price())
 tk = ''
 
 
@@ -20,15 +29,30 @@ async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
 
-# message.channel.send(name)
-# message.guild.me.edit(nick=name)
-
 @client.event
 async def on_message(message):
-    if 1:
-        name = "ETH $" + str(get_eth_price())
-        # if message.content.startswith('e'):
-        await message.guild.me.edit(nick=name)
+    name = "ETH $" + str(get_price())
+    global guildinf
+    guildinf = message.guild.me
+    print(name)
+
+    # if message.content.startswith('e'):
+    set_name.start()
+    set_status.start()
+
+
+@loop(count=None, seconds=1)
+async def set_name():
+    name = "ETH $" + str(get_price())
+    print(name)
+    await guildinf.edit(nick=name)
+
+
+@loop(count=None, seconds=1)
+async def set_status():
+    st = get_change()
+    print(st)
+    await client.change_presence(activity=discord.Game(st))
 
 
 client.run(tk)
